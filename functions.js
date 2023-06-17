b06c87572e256e1cac0155c034965dcae64bac6d
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-17 10:30:17 -0500

    worked on weeks 1-4 review

--- functions.js
+++ functions.js
@@ -27,3 +27,45 @@ console.log('running returner:', returner() );
 console.log('running actualMath with 4 & 6', actualMath( 4, 6));
 console.log('running actualMath with 4 & 6', actualMath( 2, 3));
 console.log('running actualMath with 4 & 6', actualMath( .5, 200));
+
+
+// rest of unit 4 videos
+
+console.log('------ Unit 4 weeks 1-4 review ------');
+const seatsInCar = 5;
+let passengers = [];
+let tankFull = true;
+
+function enoughSeats (){
+    console.log( 'in enoughSeats ');
+    // check if seatsInCar >= numbers of passengers
+    if (seatsInCar >= passengers.length){
+        return true;
+    } // end enough seats
+    else{
+        return false;
+    } //end not enough seats
+} 
+
+function getInCar( NameOfPassenger ) {
+    console.log('in getInCar', NameOfPassenger);
+    //push this new passenger into our passengers array
+    passengers.push (NameOfPassenger);
+    return passengers;
+} //end getInCar
+
+function readyForRoadTrip () {
+    if (enoughSeats() && tankFull ){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+getInCar ('Raf')
+getInCar ('dEv')
+getInCar ('Emma')
+getInCar ('mav')
+
+console.log('enough seats?', enoughSeats ());
+console.log( 'are we ready for the raod trip?', readyForRoadTrip());
